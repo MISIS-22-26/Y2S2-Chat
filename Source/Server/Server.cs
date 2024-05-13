c75ed632056af3369846554a06e8b294b65896dc
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 09:36:30 +0300

    Gave server ability to accept clients

--- Source/Server/Server.cs
+++ Source/Server/Server.cs
@@ -6,4 +6,5 @@ public class Server<T> : IRunnable where T : IDisposable
 {
 	protected Socket<T> socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
+	public void Accept(string ip = "localhost", int port = 8888) => socket.Open();
 }
