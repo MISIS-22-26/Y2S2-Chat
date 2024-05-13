4234819dd9ad6b322b11711a38224910a3a16429
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 11:21:46 +0300

    Server Constructor Addition

--- Source/Server/Server.cs
+++ Source/Server/Server.cs
@@ -6,4 +6,7 @@ public class Server : IRunnable
 	protected Socket socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
 	public void Accept(string ip = "localhost", int port = 8888) => socket.Open(ip, port);
+	public Server(){
+		socket = new();
+	}
 }
