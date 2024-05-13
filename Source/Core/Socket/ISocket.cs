d47260db38beec2993ca4c99468d2b310434cca3
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 09:00:38 +0300

    Made Core.ISocket.Open() accept parameters

--- Source/Core/Socket/ISocket.cs
+++ Source/Core/Socket/ISocket.cs
@@ -3,7 +3,7 @@ public interface ISocket<T> where T : IDisposable
 {
 	protected T Socket { get; set; }
 	
-	public void Open();
+	public void Open(string ip = "loclahost", int port = 8888);
 	public void Close();
 
 	public void Write();
