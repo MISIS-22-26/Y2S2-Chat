68b1c2261699a59d66ab1b83ba084f5def42efde
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 11:20:30 +0300

    Server Refinement

--- Source/Server/Server.cs
+++ Source/Server/Server.cs
@@ -1,10 +1,9 @@
 using App.Core;
-using App.Core.Socket;
 
 namespace App.Server;
-public class Server<T> : IRunnable where T : IDisposable
+public class Server : IRunnable
 {
-	protected Socket<T> socket { get; }
+	protected Socket socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
 	public void Accept(string ip = "localhost", int port = 8888) => socket.Open(ip, port);
 }
