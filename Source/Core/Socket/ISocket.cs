420dcd98233fbb2bffaa4305d2d73c9b5a391df6
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 09:03:21 +0300

    Made Core.ISocket.Open() strictly require parameters

--- Source/Core/Socket/ISocket.cs
+++ Source/Core/Socket/ISocket.cs
@@ -3,7 +3,7 @@ public interface ISocket<T> where T : IDisposable
 {
 	protected T Socket { get; set; }
 	
-	public void Open(string ip = "loclahost", int port = 8888);
+	public void Open(string ip, int port);
 	public void Close();
 
 	public void Write();
