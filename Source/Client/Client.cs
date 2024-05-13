76fee11758a30c304336167ad5ebfd377874f09e
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 23:24:58 +0300

    Client Refaactor

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -3,10 +3,14 @@ using App.Core;
 namespace App.Client;
 public class Client : IRunnable
 {
-	protected Socket socket { get; }
+	protected Socket socket 
+	{
+		get;
+	}
 	void IRunnable.Run() => throw new NotImplementedException();
 	public void Connect(string ip = "localhost", int port = 8888) => socket.Open(ip, port);
-	public Client(){
+	public Client()
+	{
 		socket = new();
 	}
 }
