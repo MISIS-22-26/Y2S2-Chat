735c8ced074ae74e1ed7d6e1d7bd62541841d1c5
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 08:50:52 +0300

    Added Socket to Client

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -1,7 +1,9 @@
 using App.Core;
+using App.Core.Socket;
 
 namespace App.Client;
 public class Client<T> : IRunnable where T : IDisposable
 {
+	protected Socket<T> socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
 }
