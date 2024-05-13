336f6c60670c3f894b5954438e8dc1b6af4922d7
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 09:38:36 +0300

    Updated Server to use new Core.Socket.Open() Implementation

--- Source/Server/Server.cs
+++ Source/Server/Server.cs
@@ -6,5 +6,5 @@ public class Server<T> : IRunnable where T : IDisposable
 {
 	protected Socket<T> socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
-	public void Accept(string ip = "localhost", int port = 8888) => socket.Open();
+	public void Accept(string ip = "localhost", int port = 8888) => socket.Open(ip, port);
 }
