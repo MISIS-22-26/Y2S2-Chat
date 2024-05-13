3c2feb1fbc262f0261800dd4374485ac10fdcb37
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 08:51:28 +0300

    Added Connect() as a facade to Socket.Open

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -6,4 +6,5 @@ public class Client<T> : IRunnable where T : IDisposable
 {
 	protected Socket<T> socket { get; }
 	void IRunnable.Run() => throw new NotImplementedException();
+	private void Connect(string ip, int port) => socket.Open();
 }
