3e1646c53f7aa44a03adce91d6c2801070d38a2c
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 08:50:24 +0300

    Made Client Generic

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -1,7 +1,7 @@
 using App.Core;
 
 namespace App.Client;
-public class Client : IRunnable
+public class Client<T> : IRunnable where T : IDisposable
 {
 	void IRunnable.Run() => throw new NotImplementedException();
 }
