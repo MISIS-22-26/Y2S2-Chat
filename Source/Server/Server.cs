55a4b095961f7409ea79273c125ebc55cb679a10
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 09:35:35 +0300

    Make server implement Core.IRunnable

--- Source/Server/Server.cs
+++ Source/Server/Server.cs
@@ -1,4 +1,7 @@
+using App.Core;
+
 namespace App.Server;
-public class Server
+public class Server<T> : IRunnable where T : IDisposable
 {
+	void IRunnable.Run() => throw new NotImplementedException();
 }
