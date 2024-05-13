589b04db311a357881f3cbe7a8e3a7501c9c0a32
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-13 07:07:39 +0300

    Fixed Bug-prone virtual keyword usage (made all methods abstract) in ISocket

--- Source/Core/Socket/ISocket.cs
+++ Source/Core/Socket/ISocket.cs
@@ -3,9 +3,9 @@ public interface ISocket<T> where T : IDisposable
 {
 	protected T Socket { get; set; }
 	
-	public virtual void Open() => throw new NotImplementedException();
-	public virtual void Close() => throw new NotImplementedException();
+	public abstract void Open();
+	public abstract void Close();
 
-	public virtual void Write() => throw new NotFiniteNumberException();
-	public virtual void Read() => throw new NotImplementedException();
+	public abstract void Write();
+	public abstract void Read();
 }
