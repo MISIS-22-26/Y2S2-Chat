398d5609265a31e3f27bf4272a62dbc4e32cdee8
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-14 03:25:25 +0300

    Errased Core.Socket Body

--- Source/Core/Socket/Socket.cs
+++ Source/Core/Socket/Socket.cs
@@ -1,12 +1,5 @@
-namespace App.Core.Socket;
-public abstract class Socket<T> : ISocket where T : IDisposable
+namespace App.Core;
+public abstract class Socket
 {
-	protected T socket { get; set; }
-	protected string ip { get; set; }
-	protected int port { get; set; }
-
-	public abstract void Close();
-	public abstract void Open(string ip, int port);
-	public abstract void Read();
-	public abstract void Write();
+	
 }
