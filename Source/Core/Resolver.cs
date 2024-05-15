2691471131e71ed9a59753c88b370f893020a1b6
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-15 15:33:11 +0300

    Synced Resolver to make it work

--- Source/Core/Resolver.cs
+++ Source/Core/Resolver.cs
@@ -2,24 +2,6 @@ using System.Net;
 
 public class Resolver(string domain)
 {
-	public bool enabled { get; private set; } = false;
 	public string domain { get; private set; } = domain;
-	IPAddress[] addresses = [];
-	public IPAddress address => addresses[0];
-	async Task Resolve()
-	{
-		IPHostEntry addresses = await Dns.GetHostEntryAsync(domain);
-		this.addresses = addresses.AddressList;
-	}
-
-	async Task Run()
-	{
-		while(enabled) await Resolve();
-	}
-		public async void Start()
-	{
-		enabled = true;
-		await Run();
-	}
-	public void Stop() => enabled = false;
+	public IPAddress address => Dns.GetHostEntry(domain).AddressList[0];
 }
