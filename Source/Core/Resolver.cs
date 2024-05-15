0383ddb7367c8434110c4e951ab5e5f22e66afc9
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-15 04:14:47 +0300

    Added Resolve Method and Related Properties to Core.Resolver

--- Source/Core/Resolver.cs
+++ Source/Core/Resolver.cs
@@ -1,4 +1,13 @@
+using System.Net;
+
 public class Resolver(string domain)
 {
-
+	public string domain { get; private set; } = domain;
+	IPAddress[] addresses = [];
+	public IPAddress address => addresses[0];
+	async Task Resolve()
+	{
+		IPHostEntry addresses = await Dns.GetHostEntryAsync(domain);
+		this.addresses = addresses.AddressList;
+	}
 }
