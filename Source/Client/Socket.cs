52bee868174810a23ce33b24fc9277501dbec271
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 14:45:43 +0300

    Made Client.Socket Async

--- Source/Client/Socket.cs
+++ Source/Client/Socket.cs
@@ -5,28 +5,28 @@ public class Socket : Core.Net.Socket.Socket
 {
 	protected override void Init() => Body.Connect(Endpoint);
 
-	protected override void Recieve()
+	protected override async Task Recieve()
 	{
 		Buffer.ReadBuffer.Flush();
-		Body.Receive(Buffer.ReadBuffer.Body);
+		await Body.ReceiveAsync(Buffer.ReadBuffer.Body);
 	}
-	protected override void Send()
+	protected override async Task Send()
 	{
-		Body.Send(Buffer.WriteBuffer.Body);
+		await Body.SendAsync(Buffer.WriteBuffer.Body);
 		Buffer.WriteBuffer.Flush();
 	}
-	void Tick()
+	async Task Tick()
 	{
 		Buffer.WriteBuffer.Body = Encoding.UTF8.GetBytes($"{Console.ReadLine()}");
-		Send();
-		Recieve();
+		await Send();
+		await Recieve();
 		Console.WriteLine(Encoding.UTF8.GetString(Buffer.ReadBuffer.Body));
 		Buffer.Flush();
 	}
-	void Run()
+	async Task Run()
 	{
 		Init();
-		while (Body.Connected) Tick();
+		while (Body.Connected) await Tick();
 	}
 	public Socket(int port, System.Net.IPAddress? address = null, int buffer_size = 1024, System.Net.Sockets.ProtocolType protocol = System.Net.Sockets.ProtocolType.Tcp) : base(port, address, buffer_size, protocol) => Run();
 }
