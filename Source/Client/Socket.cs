6e4febcaa63eb509a795e61af9bba3740c16f15f
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 12:44:11 +0300

    Reimplemented Client.Socket

--- Source/Client/Socket.cs
+++ Source/Client/Socket.cs
@@ -1,35 +1,32 @@
-using System.Net.Sockets;
 
+using System.Text;
 namespace App.Client;
-public class Socket : Core.Socket.Socket<TcpClient>
+public class Socket : Core.Net.Socket.Socket
 {
-	public override void Close()
-	{
-		socket.Close();
-	}
+	protected override void Init() => Body.Connect(Endpoint);
 
-	public override void Open(string ip, int port)
+	protected override void Recieve()
 	{
-		this.ip = ip;
-		this.port = port;
-		try
-		{
-			socket = new(ip, port);
-			Console.WriteLine("Client Connected");
-		}
-		catch (SocketException)
-		{
-			Console.WriteLine("Refused by Server...");
-		}
+		Buffer.ReadBuffer.Flush();
+		Body.Receive(Buffer.ReadBuffer.Body);
 	}
-
-	public override void Read()
+	protected override void Send()
 	{
-		throw new NotImplementedException();
+		Body.Send(Buffer.WriteBuffer.Body);
+		Buffer.WriteBuffer.Flush();
 	}
-
-	public override void Write()
+	void Tick()
 	{
-		throw new NotImplementedException();
+		Buffer.WriteBuffer.Body = Encoding.UTF8.GetBytes($"{Console.ReadLine()}");
+		Send();
+		Recieve();
+		Console.WriteLine(Encoding.UTF8.GetString(Buffer.ReadBuffer.Body));
+		Buffer.Flush();
 	}
+	void Run()
+	{
+		Init();
+		while (Body.Connected) Tick();
+	}
+	public Socket(int port, System.Net.IPAddress? address = null, int buffer_size = 1024, System.Net.Sockets.ProtocolType protocol = System.Net.Sockets.ProtocolType.Tcp) : base(port, address, buffer_size, protocol) => Run();
 }
