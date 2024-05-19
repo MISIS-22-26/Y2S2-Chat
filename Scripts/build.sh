6fc652830a9d2ff979acbbc36753893249041972
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 15:57:04 +0300

    Updated Build.sh script specifically for client

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
+
 LAUNCH_DIRECTORY=${PWD}
 WORKING_DIRECTORY=$(dirname $0)/../
 SCRIPT_NAME=$(basename $0)
 
 cd $WORKING_DIRECTORY
-case ${1} in 
-	"release")
-		dotnet publish -c release --self-contained
-	;;
-	"debug")
-		dotnet build
-	;;
-	*)
-		exit 1
-	;;
-esac
+dotnet publish -c release --self-contained -o "bin/client"
 cd $LAUNCH_DIRECTORY
