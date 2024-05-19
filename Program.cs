c86e7eb161e7f47b77da43bd4d80347521cd58b7
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 09:32:19 +0300

    Changed Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 ﻿namespace App;
 public class Program
 {
-	public static async Task Main(String[] args){
-		Core.Resolver resolver = new("localhost");
+	public static void Main(String[] args){
+		Core.Net.Resolver.Resolver resolver = new("localhost");
 		Console.WriteLine(resolver.address);
-        Core.Net.Socket socket = new(Core.Net.Mode.Server, 8888, System.Net.IPAddress.Loopback);
-		await socket.Start();
+
+        //Core.Net.Socket.Socket socket = new(Core.Net.Socket.Mode.Server, 8888, System.Net.IPAddress.Loopback);
+		//socket.Start();
 	}
 }
