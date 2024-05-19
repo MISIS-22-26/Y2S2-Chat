ffd075ac0f4c268687afc641dfdc5ddd7950fde5
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 09:33:16 +0300

    Refactored Socket to fit new Standarts, made it abstract

--- Source/Core/Socket/Socket.cs
+++ Source/Core/Socket/Socket.cs
@@ -1,167 +1,32 @@
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-namespace App.Core.Net;
-public class Socket(Mode mode, int port ,IPAddress? address = null , int buffer_size = 1024, ProtocolType protocol = ProtocolType.Tcp)
+namespace App.Core.Net.Socket;
+public abstract class Socket(int port ,IPAddress? address = null, int buffer_size = 1024, ProtocolType protocol = ProtocolType.Tcp)
 {
 
 
 
 	// Compiler wouldn't allow IPAddress.Loopbackloopback to be passed as 
-	// a default AddressType, thus coalesce expression with address fallback
-	// to null as a default are used here to hack around it.
-	IPAddress address { get; } = address ?? IPAddress.Loopback; 
+	// a default AddressType, thus coalesce expression and null default 
+	// values are used here to work around it.
+	IPAddress Address { get; } = address ?? IPAddress.Loopback; 
+	IPEndPoint Endpoint { get; } = new(address ?? IPAddress.Loopback, port);
+	System.Net.Sockets.Socket Body { get; } = new(AddressFamily.InterNetwork, SocketType.Stream, protocol);
+	Buffer Buffer { get; } = new(size: buffer_size);
 
-
-
-	
-	Mode mode { get; } = mode;
-	IPEndPoint endpoint { get; } = new(address ?? IPAddress.Loopback, port);
-
-
-
-
-
-
-
-	System.Net.Sockets.Socket socket { get; } = new(AddressFamily.InterNetwork, SocketType.Stream, protocol);
-	System.Net.Sockets.Socket connection { get; set; }
-
-
-
-
-
-
-
-
-	int buffer_size { get; } = buffer_size;
-	byte[] read_buffer { get; set; } = new byte[buffer_size];
-	byte[] write_buffer { get; set; } = new byte[buffer_size];
-	string message => Read();
-
-
-
-
-
-
-
-
-	void Open()
+	protected abstract void Init ();
+	protected abstract void Send ();
+	protected abstract void Recieve ();
+	public void Stop () 
 	{
 		try
 		{
-
-			socket.Bind(endpoint);
-			socket.Listen();
-			Console.WriteLine("Socket opened and is listening for incoming connections ...");
+			Body.Shutdown(SocketShutdown.Both); 
+			Body.Close();
 		}
 		catch
 		{
-
-			Console.WriteLine("Failure whilst openning socket..");
+			Console.WriteLine("Socket Shutdown Failure");
 		}
 	}
-	async Task Connect()
-	{
-		// This is a comment
-		try
-		{
-			await socket.ConnectAsync(endpoint);
-			Console.WriteLine("Scoket is connecting to the remote ...");
-		}
-		catch
-		{
-			Console.WriteLine("Connection failure..");
-		}
-	}
-	async Task Accept()
-	{
-		try
-		{
-			connection = await socket.AcceptAsync();
-		}
-		catch
-		{
-			Console.WriteLine(" Failed to Accept Connection");
-		}
-	}
-
-
-
-
-	async Task Send() 
-	{
-		if (write_buffer.Length == 0) return;
-		switch (mode)
-		{
-			case Mode.Server :
-				await connection.SendAsync(write_buffer);
-			break;
-			case Mode.Client :
-				await socket.SendAsync(write_buffer);
-			break;
-		}
-	}
-	async Task Recieve() 
-	{
-		if (read_buffer.Length == 0) return;
-		switch (mode)
-		{
-			case Mode.Server :
-				await connection.ReceiveAsync(read_buffer);
-			break;
-			case Mode.Client :
-				await socket.ReceiveAsync(read_buffer);
-			break;
-		}
-	}
-
-
-
-	async Task Run()
-	{
-		System.Net.Sockets.Socket? target = null;
-		switch(mode)
-		{
-			case Mode.Server: Open(); await Accept(); target = connection; break;
-			case Mode.Client: await Connect(); target = socket; break;
-		}
-		while (target != null && target.Connected) {
-			await Recieve();
-			await Send();
-			Console.WriteLine(message);;
-			Write($"{Console.ReadLine()}" );
-		}
-
-	}
-	
-	
-	
-	
-	
-	
-	public async Task Start() => await Run();
-	public void Stop()
-	{	
-		try
-		{
-			socket.Shutdown(SocketShutdown.Both);
-			socket.Disconnect(false);
-			socket.Close();
-			Console.WriteLine("Socket Closed.");
-
-		}
-		catch
-		{
-			Console.WriteLine("Failure whilst disconnecting socket..");
-		}
-	}
-
-
-
-
-
-
-	public void Write(string message) => write_buffer = Encoding.UTF8.GetBytes(message);
-	public string Read() => Encoding.UTF8.GetString(read_buffer);
 }
