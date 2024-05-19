22f7a3985ed1816d3ac9e814e7c6a0ae54679bc6
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 12:22:13 +0300

    Changed Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
 ﻿namespace App;
 public class Program
 {
-	public static void Main(String[] args){
-		Core.Net.Resolver.Resolver resolver = new("localhost");
-		Console.WriteLine(resolver.address);
-
-        //Core.Net.Socket.Socket socket = new(Core.Net.Socket.Mode.Server, 8888, System.Net.IPAddress.Loopback);
-		//socket.Start();
+	public static void Main(){
+		Console.WriteLine("Hello World");
 	}
 }
