76521d69fd6bb537370d741a2caa145a0ddbe812
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-19 12:21:09 +0300

    Fixed Core.Buffer

--- Source/Core/Buffer.cs
+++ Source/Core/Buffer.cs
@@ -1,9 +1,9 @@
 namespace App.Core;
 
 // struct since it contains mostly links to the objects in haep, so it is a good idea to place buffer in stack.
-public struct Buffer(int size = 1024)
+public class Buffer(int size = 1024)
 {
-    public int Size { get; } = size;
-	public byte[] Body { get; private set; } = new byte[size];
+    public int Size = size;
+	public byte[] Body = new byte[size];
     public void Flush() => Body = new byte[Size];
 }
