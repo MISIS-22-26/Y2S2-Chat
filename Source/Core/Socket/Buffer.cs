c19183336fd9441b6caf72e6c5ea3d776fc7d7e2
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-21 14:46:50 +0300

    Reconcidered Buffer naming Desicions

--- Source/Core/Socket/Buffer.cs
+++ Source/Core/Socket/Buffer.cs
@@ -1,11 +1,11 @@
 namespace App.Core.Net.Socket;
 public class Buffer(int size = 1024)
 {
-	public Core.Buffer ReadBuffer = new(size);
-	public Core.Buffer WriteBuffer = new(size);
+	public Core.Buffer Read = new(size);
+	public Core.Buffer Write = new(size);
 	public void Flush()
 	{
-		ReadBuffer.Flush();
-		WriteBuffer.Flush();
+		Read.Flush();
+		Write.Flush();
 	}
 }
