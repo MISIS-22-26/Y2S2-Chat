99d3b77fe75d007bab125312044f5bddbcc71a10
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-21 14:49:12 +0300

    Made Core.Buffer Generic

--- Source/Core/Buffer.cs
+++ Source/Core/Buffer.cs
@@ -1,9 +1,9 @@
 namespace App.Core;
 
 // struct since it contains mostly links to the objects in haep, so it is a good idea to place buffer in stack.
-public class Buffer(int size = 1024)
+public class Buffer<T>(int size = 1024)
 {
     public int Size = size;
-	public byte[] Body = new byte[size];
-    public void Flush() => Body = new byte[Size];
+	public T[] Body = new T[size];
+    public void Flush() => Body = new T[Size];
 }
