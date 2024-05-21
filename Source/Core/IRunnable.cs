ab1d0259590df5a82ded3d61fd44e9dc7563e19a
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 00:33:14 +0300

    Refactored IRunnable with tabs

--- Source/Core/IRunnable.cs
+++ Source/Core/IRunnable.cs
@@ -1,27 +1,27 @@
 namespace App.Core;
 public interface IRunnable
 {
-    protected Thread? Thread { get; set; }
-    public bool Shutdown { get; protected set; }
-    public bool Startup { get; protected set; }
-    public bool Running { get; protected set; }
-    public void Start()
-    {
-        if (Running || Startup || Shutdown) return;
-        
-        Startup = true;
-        Thread ??= new(new ThreadStart(Run)); // If thread is null
-        Thread.Start();
-        Startup = false;
-    }
-    protected void Tick();
-    private void Run()
-    {
-        Running = true;
-        while(!Shutdown) Tick();
-        Running = false;
-        
-        Shutdown = false;
-    }
-    public void Stop() => Shutdown = true;
+	protected Thread? Thread { get; set; }
+	public bool Shutdown { get; protected set; }
+	public bool Startup { get; protected set; }
+	public bool Running { get; protected set; }
+	public void Start()
+	{   
+		if (Running || Startup || Shutdown) return;
+		
+		Startup = true;
+		Thread ??= new(new ThreadStart(Run)); // If thread is null
+		Thread.Start();
+		Startup = false;
+	}
+	protected void Tick();
+	private void Run()
+	{
+		Running = true;
+		while(!Shutdown) Tick();
+		Running = false;
+		
+		Shutdown = false;
+	}
+	public void Stop() => Shutdown = true;
 }
