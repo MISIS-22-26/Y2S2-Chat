9283b63f155f3bc263ace73dfc35dc233b2d6b47
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 00:37:44 +0300

    Made Writer contain Bufferized property rather than being itself Bufferized

--- Source/Core/IO/Writer.cs
+++ Source/Core/IO/Writer.cs
@@ -1,17 +1,24 @@
 using App.Core.Bufferization;
 
 namespace App.Core.IO;
-public abstract class Writer(int buffer_size = 1024) : Bufferized<byte>(buffer_size), IRunnable
+public abstract class Writer(int buffer_size = 1024) : IRunnable
 {
+    public Buffer Buffer { get; set; } = new(size: buffer_size);
+    bool Writing { get; set; } = false;
+    protected abstract void Write();
+
+
+
+
+    /* IRunnable Implementation */
+    Thread? IRunnable.Thread { get; set; } = null;
     bool IRunnable.Shutdown { get; set; } = false;
     bool IRunnable.Startup { get; set; } = false;
     bool IRunnable.Running { get; set; } = false;
-    bool Writing { get; set; } = false;
     void IRunnable.Tick() {
         Writing = true;
         Write();
         Writing = false;
     }
     
-    protected abstract void Write();
 }
