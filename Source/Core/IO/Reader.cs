bf3c93c1de294b1b5dba828a287973d9498e9fba
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 00:36:49 +0300

    Made Reader Contain Bufferized Property (Buffer) instead of being Bufferized itself

--- Source/Core/IO/Reader.cs
+++ Source/Core/IO/Reader.cs
@@ -1,17 +1,26 @@
-using App.Core.Bufferization;
 
 namespace App.Core.IO;
-public abstract class Reader(int buffer_size = 1024) : Bufferized<byte>(buffer_size), IRunnable
+public abstract class Reader(int buffer_size = 1024) : IRunnable
 {
-    bool IRunnable.Shutdown { get; set; } = false;
-    bool IRunnable.Startup { get; set; } = false;
-    bool IRunnable.Running { get; set; } = false;
-    bool Reading { get; set; } = false;
-    void IRunnable.Tick() {
-        Reading = true;
-        Read();
-        Reading = false;
-    }
-    protected abstract void Read();
+	public Buffer Buffer { get; set; } = new(size: buffer_size);
+	bool Reading { get; set; } = false;
+	protected abstract void Read();
 
+
+
+
+
+
+
+	/* IRunnable Implementation */
+
+	Thread? IRunnable.Thread { get; set; } = null;
+	bool IRunnable.Shutdown { get; set; } = false;
+	bool IRunnable.Startup { get; set; } = false;
+	bool IRunnable.Running { get; set; } = false;
+	void IRunnable.Tick() {
+		Reading = true;
+		Read();
+		Reading = false;
+	}
 }
