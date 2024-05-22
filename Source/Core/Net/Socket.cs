b3a7b853353c9d7b4f253f660b5a142399709d70
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 09:28:54 +0300

    Refactored Socket

--- Source/Core/Net/Socket.cs
+++ Source/Core/Net/Socket.cs
@@ -6,28 +6,20 @@ using App.Core.IO;
 namespace App.Core.Net;
 public abstract class Socket(IPAddress? address, int port, ProtocolType protocol, Reader reader, Writer writer)
 {
-	// Compiler wouldn't allow IPAddress.Loopbackloopback to be passed as 
-	// a default AddressType, thus coalesce expression and null default 
-	// values are used here to work around it
 	protected IPEndPoint Endpoint { get; } = new(address ?? IPAddress.Loopback, port);
 	protected System.Net.Sockets.Socket Body { get; } = new(AddressFamily.InterNetwork, SocketType.Stream, protocol);
-
-
-
-
-
 	protected List<IRunnable> Proccesses { get; } = [];
+
+
+	
 	public Reader Reader { get; } = reader;
 	public Writer Writer { get; } = writer;
 	
 	
 
-	// User-Defined Preparations before actual Startup.
-	protected abstract void Setup();
-	
-	// Core-Defined Preparations with followed up startup
-	public void Init (){
-		Setup();
+	protected abstract void Init();
+	public void Open (){
+		Init();
 		Proccesses.Add(Reader);
 		Proccesses.Add(Writer);
 
