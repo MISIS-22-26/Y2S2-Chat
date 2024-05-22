f12f18d23f44b71a9cfdc3a10bee5dd7db6ff915
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 07:11:09 +0300

    Core.Bufferization.Bufferized.Busy naming fix

--- Source/Core/Bufferization/Bufferized.cs
+++ Source/Core/Bufferization/Bufferized.cs
@@ -3,6 +3,6 @@ namespace App.Core.Bufferization;
 /// Default Implementation of IBufferized
 public class Bufferized<T>(int buffer_size) : IBufferized<T>
 {
-    bool IBufferized<T>.busy { get; set;}
+    bool IBufferized<T>.Busy { get; set;}
     Buffer<T> IBufferized<T>.buffer { get; set; } = new(buffer_size);
 }
