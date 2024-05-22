318b2e1038913db8a5f9ad3367051c182cb1462b
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 07:12:01 +0300

    Reconsidered Buffer Implementation and Design

--- Source/Core/Bufferization/Buffer.cs
+++ Source/Core/Bufferization/Buffer.cs
@@ -1,9 +1,11 @@
+using System.Numerics;
+
 namespace App.Core.Bufferization;
 
 // struct since it contains mostly links to the objects in haep, so it is a good idea to place buffer in stack.
-public class Buffer<T>(int size = 1024)
+public class Buffer<T>(int size)
 {
-    public int Size = size;
-	public T[] Body = new T[size];
-    public void Flush() => Body = new T[Size];
+    public int Size { get; } = size;
+	public T[] Body { get; } = new T[size];
+    public virtual void Flush() => throw new NotImplementedException();
 }
