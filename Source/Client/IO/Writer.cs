30f93238fd1691e683cf12d2838e951a0ae0c22d
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 09:32:47 +0300

    Updated Client.IO to new API

---
+++ Source/Client/IO/Writer.cs
@@ -0,0 +1,10 @@
+namespace App.Client.IO;
+public class Writer(System.Net.Sockets.Socket socket, int buffer_size) : Core.IO.Writer(buffer_size)
+{
+    private System.Net.Sockets.Socket Socket { get; } = socket;
+    protected override void Write()
+    {
+        if (!Socket.Connected) return;
+        Socket.Send(Buffer.Body);
+    }
+}
