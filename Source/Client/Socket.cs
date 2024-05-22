5b625195a149e288418ec78569a64ee2f7031b7a
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 09:31:52 +0300

    Implemented Client Socket with updated API

--- Source/Client/Socket.cs
+++ Source/Client/Socket.cs
@@ -1,41 +1,8 @@
 
-using System.Text;
 namespace App.Client;
-public class Socket(System.Net.IPAddress? address, int port, System.Net.Sockets.ProtocolType protocol) : Core.Net.Socket.Socket(port, address, buffer_size, protocol)
+public class Socket(System.Net.IPAddress? address, int port, System.Net.Sockets.ProtocolType protocol, IO.Reader reader, IO.Writer writer) : Core.Net.Socket(address,port,protocol,reader,writer)
 {
+	public byte[] Read() => Reader.Buffer.Body;
+	public void Write(ref byte[] data) => Writer.Buffer.Body = data;
 	protected override void Init() => Body.Connect(Endpoint);
-
-	protected override async Task Recieve()
-	{
-		Buffer.ReadBuffer.Flush();
-		await Body.ReceiveAsync(Buffer.ReadBuffer.Body);
-	}
-	protected override async Task Send()
-	{
-		await Body.SendAsync(Buffer.WriteBuffer.Body);
-		Buffer.WriteBuffer.Flush();
-	}
-	async Task Tick()
-	{
-		string output = $"{Console.ReadLine()}";
-		if(output != "")
-		{
-			Buffer.WriteBuffer.Body = Encoding.UTF8.GetBytes(output);
-			await Send();
-			Buffer.WriteBuffer.Flush();
-		}
-		await Recieve();
-
-		string input = Encoding.UTF8.GetString(Buffer.ReadBuffer.Body);
-		if(input != "")
-		{
-			Console.WriteLine(input);
-			Buffer.WriteBuffer.Flush();
-		}
-	}
-	public async Task Run()
-	{
-		Init();
-		while (Body.Connected) await Tick();
-	}
 }
