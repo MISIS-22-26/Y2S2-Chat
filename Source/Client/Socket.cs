4a1c1853812fcdfb3ef125e914b168547a6bdef6
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 19:50:47 +0300

    Client.Socket implementation on new API

--- Source/Client/Socket.cs
+++ Source/Client/Socket.cs
@@ -1,8 +1,13 @@
 
 namespace App.Client;
-public class Socket(System.Net.IPAddress? address, int port, System.Net.Sockets.ProtocolType protocol, IO.Reader reader, IO.Writer writer) : Core.Net.Socket(address,port,protocol,reader,writer)
+public class Socket(System.Net.IPAddress? address, int port, System.Net.Sockets.ProtocolType protocol, int buffer_size) : Core.Net.Socket(address,port,protocol,buffer_size)
 {
 	public byte[] Read() => Reader.Buffer.Body;
 	public void Write(ref byte[] data) => Writer.Buffer.Body = data;
-	protected override void Init() => Body.Connect(Endpoint);
+	protected override void Init()
+	{
+		Reader = new IO.Reader(Body, BufferSize);
+		Writer = new IO.Writer(Body, BufferSize);
+		Body.Connect(Endpoint);
+	}
 }
