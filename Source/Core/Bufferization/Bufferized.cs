42095a8a309cd4b1501a0e2e29751d3e766ce6f4
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-22 07:10:20 +0300

    Removed default buffer size value from the constructor of the default IBufferized Implementation

--- Source/Core/Bufferization/Bufferized.cs
+++ Source/Core/Bufferization/Bufferized.cs
@@ -1,7 +1,7 @@
 namespace App.Core.Bufferization;
 
 /// Default Implementation of IBufferized
-public class Bufferized<T>(int buffer_size = 1024) : IBufferized<T>
+public class Bufferized<T>(int buffer_size) : IBufferized<T>
 {
     bool IBufferized<T>.busy { get; set;}
     Buffer<T> IBufferized<T>.buffer { get; set; } = new(buffer_size);
