0a9c8a83c6ebd2d8f66787be80cf072a9d0a39d1
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 05:54:05 +0300

    Generalized Core.IO.Bufferizer.cs

--- Source/Core/IO/Buferizer.cs
+++ Source/Core/IO/Buferizer.cs
@@ -1,5 +1,17 @@
 namespace App.Core.IO;
-public abstract class Bufferizer(int buffer_size) : Node
+public abstract class Bufferizer<T>(int buffer_size) : Node
 {
-    public Bufferization.Buffer<byte> Buffer { get; } = new(buffer_size);
+    protected int Size { get; } = buffer_size; // Default size for each buffer in Buffers
+
+	// Buffer of Buffers
+	public List<Bufferization.Buffer<T>> Buffers { get; } = [ ];
+	// Main Buffer in case there are more than one buffer in Buffers
+	public Bufferization.Buffer<T> Buffer 
+	{
+		get => Buffers[0];
+		private set => Buffers[0] = value;
+	}
+	protected override void Setup(){
+		Buffers.Add(new(Size)); // If you use Bufferized class you probably want at least 1 buffer existing.. duh.
+	}
 }
