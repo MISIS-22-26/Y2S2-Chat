58f3298afb513802f900ebfb2b9c2e77cccf4c4f
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 11:32:16 +0300

    Core.Socket IO Proccesses fix

--- Source/Core/Net/Socket.cs
+++ Source/Core/Net/Socket.cs
@@ -6,7 +6,7 @@ using App.Core.IO;
 namespace App.Core.Net;
 public abstract class Socket
 {
-    public Socket(IPAddress address, int port, ProtocolType protocol, int buffer_size)
+	public Socket(IPAddress address, int port, ProtocolType protocol, int buffer_size, Reader<byte> reader, Writer<byte> writer)
 	{
 		BufferSize = buffer_size;
 		
@@ -15,24 +15,15 @@ public abstract class Socket
 	
 		Endpoint = endpoint;
 		Body = socket;
-		Reader = new(buffer_size);
-		Writer = new(buffer_size);
 
-		((Multithreading.IRunnable) this).Initialize();
+		Reader = reader;
+		Writer = writer;
+
+		Proccesses.Add(Reader);
+		Proccesses.Add(Writer);
+
 	}
-	public Socket(System.Net.Sockets.Socket socket, int buffer_size)
-	{
-		BufferSize = buffer_size;
-		Body = socket;
-		Endpoint = socket.LocalEndPoint;
-		Reader = new(buffer_size);
-		Writer = new(buffer_size);
-
-		((Multithreading.IRunnable) this).Initialize();
-	}
-
-
-	public EndPoint? Endpoint { get; protected set; }
+	public EndPoint Endpoint { get; protected set; }
     public System.Net.Sockets.Socket Body { get; }
 
 
@@ -47,11 +38,6 @@ public abstract class Socket
 	public void Open()
 	{
 		Init();
-
-		 // If null throw null ref exception
-		Proccesses.Add(Reader ?? throw new NullReferenceException("Reader Can't be null"));
-		Proccesses.Add(Reader ?? throw new NullReferenceException("Writer Can't be null"));
-
 		foreach(var proccess in Proccesses) proccess.Start();
 	}
 	public void Close () 
