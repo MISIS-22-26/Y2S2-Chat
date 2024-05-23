d788059a592d7e04899ebf2400642c1c537b3f11
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 06:49:47 +0300

    Modernized Core.Net.Socket

--- Source/Core/Net/Socket.cs
+++ Source/Core/Net/Socket.cs
@@ -4,17 +4,20 @@ using App.Core.IO;
 
 
 namespace App.Core.Net;
-public abstract class Socket(IPAddress? address, int port, ProtocolType protocol, int buffer_size)
+public abstract class Socket(System.Net.Sockets.Socket socket, int buffer_size)
 {
-	public int BufferSize { get; } = buffer_size;
-	protected IPEndPoint Endpoint { get; } = new(address ?? IPAddress.Loopback, port);
-	public System.Net.Sockets.Socket Body { get;} = new(AddressFamily.InterNetwork, SocketType.Stream, protocol);
-	protected List<IRunnable> Proccesses { get; } = [];
+    public Socket(IPAddress? address, int port, ProtocolType protocol, int buffer_size) : this(new(new IPEndPoint(address ?? IPAddress.Loopback, port).AddressFamily , SocketType.Stream, protocol), buffer_size) {}
+
+
+
+    public int BufferSize { get; } = buffer_size;
+    public System.Net.Sockets.Socket Body { get; } = socket;
+    protected List<IRunnable> Proccesses { get; } = [];
 
 
 	
-	public Reader<byte> Reader { get; protected set; }
-	public Writer<byte> Writer { get; protected set; }
+	public Reader<byte>? Reader { get; protected set; } = null;
+	public Writer<byte>? Writer { get; protected set; } = null;
 	
 	
 
