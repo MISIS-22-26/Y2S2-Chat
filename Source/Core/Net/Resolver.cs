7e1f5618f993f13aaaa5033dccb17945fcb12302
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 06:59:23 +0300

    Resolver Warning Fixes

--- Source/Core/Net/Resolver.cs
+++ Source/Core/Net/Resolver.cs
@@ -2,6 +2,6 @@ using System.Net;
 namespace App.Core.Net;
 public class Resolver(string domain)
 {
-	public string domain { get; set; } = domain;
-	public IPAddress address => Dns.GetHostEntry(domain).AddressList[0];
+	public string Domain { get; set; } = domain;
+	public IPAddress Address => Dns.GetHostEntry(Domain).AddressList[0];
 }
