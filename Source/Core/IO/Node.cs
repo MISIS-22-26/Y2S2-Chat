e47b43f3156ea3ab14b5d1fe10d2fecb36d52378
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 05:53:28 +0300

    Documentation of Node.cs

--- Source/Core/IO/Node.cs
+++ Source/Core/IO/Node.cs
@@ -10,6 +10,7 @@ public class Node : IRunnable
 	bool IRunnable.Running { get; set; } = false;
 	void IRunnable.Init() => Setup();
     void IRunnable.Tick() => Operate();
+	// User-Defined method to be called during initialization
 	protected virtual void Setup() {}
 
 	/* Non-Implemented */
