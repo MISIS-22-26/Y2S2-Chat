d27e6c6b1923accdd52e48530ca0700467ad3bf6
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 05:47:03 +0300

    Added Ability for IRunnable and Node to add code during initialization via override

--- Source/Core/IO/Node.cs
+++ Source/Core/IO/Node.cs
@@ -4,11 +4,13 @@ public class Node : IRunnable
     /* IRunnable Implementation */
 
     Thread? IRunnable.Thread { get; set; } = null;
+	bool IRunnable.Initializing { get; set; } = false;
 	bool IRunnable.Shutdown { get; set; } = false;
 	bool IRunnable.Startup { get; set; } = false;
 	bool IRunnable.Running { get; set; } = false;
+	void IRunnable.Init() => Setup();
     void IRunnable.Tick() => Operate();
-
+	protected virtual void Setup() {}
 
 	/* Non-Implemented */
 	protected virtual void Operate() => throw new NotImplementedException();
