0ccf5e946a3f2c1ed2c6994f04b6a8fbf7f6f780
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-23 07:08:26 +0300

    Made Resolver an IRunnable Proccess

--- Source/Core/Net/Resolver.cs
+++ Source/Core/Net/Resolver.cs
@@ -1,7 +1,18 @@
 using System.Net;
 namespace App.Core.Net;
-public class Resolver(string domain)
+public class Resolver(string domain) : IRunnable
 {
 	public string Domain { get; set; } = domain;
-	public IPAddress Address => Dns.GetHostEntry(Domain).AddressList[0];
+	public IPAddress[] Addresses { get; private set; } = [];
+	public IPAddress Address => Addresses[0];
+
+	/* IRunnable Implementation */
+
+    Thread? IRunnable.Thread { get; set; } = null;
+	bool IRunnable.Initializing { get; set; } = false;
+	bool IRunnable.Shutdown { get; set; } = false;
+	bool IRunnable.Startup { get; set; } = false;
+	bool IRunnable.Running { get; set; } = false;
+	void IRunnable.Init() {}
+    void IRunnable.Tick() => Addresses = Dns.GetHostEntry(Domain).AddressList;
 }
