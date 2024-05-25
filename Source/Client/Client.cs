421f0ee54d741fbb69bba42759a0bf7b7e9dcae1
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 04:51:35 +0300

    Fixed Client

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -1,8 +1,6 @@
-using System.Net;
-
 namespace App.Client;
 public class Client(System.Net.IPAddress address, int port, System.Net.Sockets.ProtocolType protocol, int buffer_size = 1024)
 {
-	Socket socket = new(address, port, protocol, buffer_size);
+    private readonly Socket socket = new(address, port, protocol, buffer_size);
 	public void Start() => socket.Open();
 }
