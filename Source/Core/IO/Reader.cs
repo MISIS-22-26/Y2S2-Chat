3a9578f1aa3956704232e833613bb9890743fe11
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 06:47:26 +0300

    IO new Proccess:IRunnable implementation

--- Source/Core/IO/Reader.cs
+++ Source/Core/IO/Reader.cs
@@ -1,5 +1,5 @@
 namespace App.Core.IO;
-public class Reader<T>(int buffer_size) : Bufferized<T>(buffer_size)
+public class Reader<T>(int buffer_size, string proccess_name = "Bufferized Reader") : Bufferized<T>(buffer_size, proccess_name)
 {
 	protected virtual void Read() {}
     protected override void Operate() => Read();
