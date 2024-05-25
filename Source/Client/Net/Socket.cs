12cec336b172248ed7eff9d31e347ebe73c7ecef
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 05:46:01 +0300

    Fixed Socket to use patched API

---
+++ Source/Client/Net/Socket.cs
@@ -0,0 +1,13 @@
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace App.Client;
+public class Socket
+(IPAddress address,int port,ProtocolType protocol,int buffer_size) : Core.Net.Socket<IO.Reader,IO.Writer>
+( address, port, protocol, new IO.Reader(buffer_size), new IO.Writer(buffer_size) )
+{
+	public byte[] Read() => Reader.Buffer.Body;
+	public void Write(ref byte[] data) => Writer.Buffer.Body = data;
+	protected override void Init() => Body.Connect(Endpoint);
+}
