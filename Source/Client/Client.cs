49d0992535967d8bd69e81d3776c3ebf550009e2
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 04:49:13 +0300

    Made Client.address non-nullable

--- Source/Client/Client.cs
+++ Source/Client/Client.cs
@@ -1,7 +1,7 @@
 using System.Net;
 
 namespace App.Client;
-public class Client(IPAddress? address, int port, System.Net.Sockets.ProtocolType protocol, int buffer_size = 1024)
+public class Client(System.Net.IPAddress address, int port, System.Net.Sockets.ProtocolType protocol, int buffer_size = 1024)
 {
 	Socket socket = new(address, port, protocol, buffer_size);
 	public void Start() => socket.Open();
