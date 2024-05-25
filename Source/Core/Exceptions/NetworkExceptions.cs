1f22a2f217a29728d8f7fc48b0c0da581f9ced83
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 06:04:14 +0300

    Added exception thrown on connection to null-target

--- Source/Core/Exceptions/NetworkExceptions.cs
+++ Source/Core/Exceptions/NetworkExceptions.cs
@@ -1,2 +1,3 @@
 namespace App.Core.Exceptions;
 public class DisconnectedException(string message="Disconnected.") : IOException(message){}
+public class NullConnectionException(string message="Connecting to null is prohibited.") : IOException(message) {}
