a52a1d118f1ad2d83685816dbfb460b0fda2c55e
Nosikov Daniil Grigoryevich <danilanosikov@yandex.ru>
2024-05-25 07:28:28 +0300

    Entrypoint Refactor

--- Source/Client/Entrypoint.cs
+++ Source/Client/Entrypoint.cs
@@ -1,6 +1,5 @@
-using System.Net;
 namespace App.Client;
 public class Entrypoint
 {
-	public static void Main() => new Client(Dns.GetHostEntry("localhost").AddressList[0], 8888, System.Net.Sockets.ProtocolType.Tcp).Start();
+	public static void Main() => _ = new Client("localhost", 8080, System.Net.Sockets.ProtocolType.Tcp);
 }
